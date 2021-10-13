86aca270faa83f9938d1e7555eed5651b8dee27a
p.ghirlanda <p.ghirlanda@net.estia.fr>
2021-10-13 11:34:15 +0200

    test

--- grails-app/domain/annonces/estia/Annonce.groovy
+++ grails-app/domain/annonces/estia/Annonce.groovy
@@ -10,11 +10,10 @@ class Annonce {
     Date dateCreated
     Date lastUpdated
 
+    String adeline
+
     static belongsTo = [author: User]
-
     static hasMany = [illustrations: Illustration]
-
-
     static constraints = {
         title nullable: false, blank: false, size: 5..100
         description nullable: true, blank: true
