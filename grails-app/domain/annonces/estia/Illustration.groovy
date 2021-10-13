8db1cb556bc4b74937ca36e806bf1947e95d152a
p.ghirlanda <p.ghirlanda@net.estia.fr>
2021-10-13 11:37:25 +0200

    test

--- grails-app/domain/annonces/estia/Illustration.groovy
+++ grails-app/domain/annonces/estia/Illustration.groovy
@@ -3,9 +3,8 @@ package annonces.estia
 class Illustration {
 
     String filename
-
+    String coucou
     static belongsTo = [annonce: Annonce]
-
     static constraints = {
         filename nullable: false, blank: false
     }
